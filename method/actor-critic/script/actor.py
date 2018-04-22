8d1523e9585c293d4dc2176160a796da121c6844
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-04-22 19:33:55 +1000

    fwd prop in actor

--- method/actor-critic/script/actor.py
+++ method/actor-critic/script/actor.py
@@ -7,30 +7,34 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'  ## To deactivate SSE Warnings
 
 class ActorNeuralNetwork():## aka Policy Network
     def __init__(self, input_dim, output_dim):
-        hidden_dim = 200 # := n_hidden_units
-        eta = 1e-3
-        decay = 0.99
-
         # Placeholders for passing:
         # input state, self._x;
         # predicted action, self.tf.y;
         # corresponding reward, self.tf_r;
+        hidden_dim = 200 # := n_hidden_units
+
         self._x = tf.placeholder(dtype=tf.float32, shape=[None, input_dim], name="x")
         self._y = tf.placeholder(dtype=tf.float32, shape=[None, output_dim], name="y")
         self._returns = tf.placeholder(dtype=tf.float32, shape=[None, 1], name="returns")
 
-        # Weights; initialized using Xavier initialization
+        ## Weights; initialized using Xavier initialization
         xavier_l1 = tf.truncated_normal_initializer(mean=0, stddev=1. / np.sqrt(input_dim), dtype=tf.float32)
         self.W1 = tf.get_variable("W1", [input_dim, hidden_dim], initializer=xavier_l1)
 
         xavier_l2 = tf.truncated_normal_initializer(mean=0, stddev=1. / np.sqrt(hidden_dim), dtype=tf.float32)
         self.W2 = tf.get_variable("W2", [hidden_dim, output_dim], initializer=xavier_l2)
 
-        self.tf_aprob = self._policy_forward(self._x)
+        ## forward prop operator
+        self._forward_prop_op = self._policy_forward(self._x)
+
+        ## loss
+        loss = tf.nn.l2_loss(self._y - self.tf_aprob)
 
         # Define Optimizer, compute and apply gradients
-        loss = tf.nn.l2_loss(self._y - self.tf_aprob)
+        eta = 1e-3
+        decay = 0.99
         optimizer = tf.train.RMSPropOptimizer(eta, decay=decay)
+
         tf_grads = optimizer.compute_gradients(loss, var_list=tf.trainable_variables(), grad_loss=self._returns)
         self._train_op = optimizer.apply_gradients(tf_grads)
 
@@ -47,10 +51,15 @@ class ActorNeuralNetwork():## aka Policy Network
 
         return self._session.run(self._train_op, feed)
 
-    def _policy_forward(self, x): #x ~ [1,D]
-        # a two-layer NN: to provide the probability of moving the paddle UP, given an input observation state
-        # The network takes the processed image of the state of the game and
-        # generates a single number denoting the probability of moving the paddle UP.
+    def forward_prop(self, x):
+        # to compute the probability:
+        # because In TensorFlow, the network graph is computed only in the TensorFlow session,
+        feed = {self._x: np.reshape(x, (1, -1))}
+        aprob = self.session.run(self._forward_prop_op, feed);
+
+        return aprob
+
+    def _forward_prop(self, x):
         h = tf.matmul(x, self.W1)
         h = tf.nn.relu(h)
 
--- method/actor-critic/script/agent.py
+++ method/actor-critic/script/agent.py
@@ -5,17 +5,20 @@ from critic import CriticNeuralNetwork
 
 class ActorCriticAgent():
     def __init__(self, n_actions, initial_observation):
-        input_dim = initial_observation.size
-        output_dim = n_actions
-
-        self._actor_net = ActorNeuralNetwork(input_dim, output_dim)
-        self._critic_net = CriticNeuralNetwork()
         self.n_actions = n_actions
+
+        self._actor_net = ActorNeuralNetwork(input_dim=initial_observation.size,
+                                             output_dim=n_actions)
+
+        self._critic_net = CriticNeuralNetwork()
 
     def act(self, obs):
         probs = self._actor_net.forward_prop(obs)
         idx = np.random.choice(self.n_actions, p=probs)
 
+        ## "fake" true label
+        ## assume whatever action taken is the right action to take, later
+        ## this will be modulated by the Advantage, e.g. return
         labels = np.zeros_like(probs)
         labels[idx] = 1
 
